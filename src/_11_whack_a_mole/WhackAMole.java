76089c49d3b7fc1418df520c1b97ffc6c7278415
370257 <370257@0338S-S-R0B6ZH5.sdcs.local>
2022-12-01 15:27:56 -0800

    finish piggy

---
+++ src/_11_whack_a_mole/WhackAMole.java
@@ -0,0 +1,23 @@
+package _11_whack_a_mole;
+
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class WhackAMole {
+	int rand = new Random().nextInt(15);
+	JFrame joe = new JFrame("Whack A Button for Fame and Glory");
+	JPanel biden = new JPanel();
+	static void drawButtons(int rand) {
+		for(int i=0;i<rand+1;i++) {
+		JButton num = new JButton();
+		}
+}
+	public void run() {
+		joe.setSize(600,600);
+		joe.add(biden);
+	}
+}
